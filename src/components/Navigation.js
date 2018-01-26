95ba1980fd3cec734f69f1a9693c7366357e49df
atiffany <abby.tiffany@gmail.com>
2018-01-25 17:13:15 -0800

    updates to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-browser-router';
 import './Navigation.css';
+import logo from './largeGnome.png';
 
 class Navigation extends React.Component {
     render() {
         return (
             <div className = "header">
+                <div className = "header--title">
+                    <Link to = '/'>
+                    <div className = "header--title--text">Tome Gnome</div>
+                    <img className = "header--title--image" src ={logo} alt = "logo"/>
+                    </Link>
+                </div>
                 <div className = "header--text"> 
-                    <Link to = '/'>Tome Gnome</Link>
-                    <Link to = '/enterbook'>Enter A New Book</Link>
-                    <Link to = '/displaybooks'>Books I've Read</Link>
-                    <Link to = '/displaybooks'>Books To Read Next</Link>
+                    <Link to = '/enterbook' className = "header--text">Enter A New Book</Link>
+                    <Link to = '/displaybooks' className = "header--text">Books I've Read</Link>
+                    <Link to = '/displaybooks' className = "header--text">Books To Read Next</Link>
                 </div>
             </div>
         );
